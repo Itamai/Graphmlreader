24af3acf4796b5670e1cc5a6ce22bd2290a1cf81
Itamai <53093696+Itamai@users.noreply.github.com>
2019-12-28 19:50:50 +0100

    Update Graph.java

--- Graphmlreader/src/com/company/Graph.java
+++ Graphmlreader/src/com/company/Graph.java
@@ -1,14 +1,6 @@
 package com.company;
 
-import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import java.io.File;
-import java.io.IOException;
-import java.util.Scanner;
 
 public class Graph {
 
