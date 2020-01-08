961f1aa09514249c85de94bbc41dae481eece7be
Itamai <53093696+Itamai@users.noreply.github.com>
2020-01-08 18:32:08 +0100

    Update Main.java

--- Graphmlreader/src/com/company/Main.java
+++ Graphmlreader/src/com/company/Main.java
@@ -1,37 +1,22 @@
 package com.company;
 
 import javax.xml.parsers.*;
-
-import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
 import java.io.*;
-import java.util.Scanner;
+
+
 
 public class Main
 {
-public static Document doc;
-public static String filepath;
+    public static void main(String argv[]) throws SAXException, ParserConfigurationException, IOException {
 
-    public static void main(String argv[]) throws IOException, SAXException, ParserConfigurationException {
-
-
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Enter input file path: ");
-        filepath = scan.next();
-        scan.close();
-        File file = new File("C:\\Users\\ati\\Documents\\small_graph.graphml");
-        if(file.exists() && !file.isDirectory()) {
-            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-            DocumentBuilder db = dbf.newDocumentBuilder();
-            doc = db.parse(file);
-        }
-        else
-            System.out.println("No or Wrong File provided");
-
-        Graph test = new Graph();
-        test.nodelist();
-        test.edgelist();
-        test.destination();
+        Graph graphml = new Graph();
+        graphml.readFile("C:\\Users\\ati\\Documents\\small_graph.graphml");
+        GraphProperties test = new GraphProperties(graphml);
+        test.showStatus();
+        /*graphml.printAllNodes();
+        graphml.printAllEdges();*/
+        test.shortestPath(Integer.parseInt(String.valueOf(1)),Integer.parseInt(String.valueOf(14)));
 
     }
-}
+}        
