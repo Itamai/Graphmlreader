151ffa6240e06732d079c4bb85ca51aab2120b7e
Itamai <53093696+Itamai@users.noreply.github.com>
2020-01-08 18:33:33 +0100

    Create Node.java

---
+++ Graphmlreader/src/com/company/Node.java
@@ -0,0 +1,13 @@
+public class Node {
+    private int nodeId;
+
+    public Node(int nodeId) {
+
+        this.nodeId = nodeId;
+    }
+
+    public int getId() {
+
+        return nodeId;
+    }
+}
